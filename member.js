817ce63344ca6209eff32c5ede30a88c5e7010e7
Siddharth sarkar <91683625+hey-sid29@users.noreply.github.com>
2023-08-13 07:47:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    member.style.display = "block";
+    skills.style.display = "none";
+}
